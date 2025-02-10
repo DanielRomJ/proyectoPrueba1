731b78699177066a0c2ec9b43d5c45edc115591c
danielromero4 <daniel-esteban-romero@hotmail.com>
2025-02-10 14:30:19 -0500

    Correccion errores

--- 06_PACKAGES/FA_QVNTS_PEDIDO/UNIT_TEST/TEST_FA_QVNTS_PEDIDO.insertar_pedido.sql
+++ 06_PACKAGES/FA_QVNTS_PEDIDO/UNIT_TEST/TEST_FA_QVNTS_PEDIDO.insertar_pedido.sql
@@ -1,8 +1,14 @@
+DECLARE
+    
+    v_PEDIDO_PEDIDO FA_QVNTS_PEDIDO.PEDIDO_PEDIDO%TYPE;
+  
 BEGIN
-    -- Insertar un nuevo cliente
+	v_PEDIDO_PEDIDO:= 1:
+    -- Insertar un nuevo pedido
     FA_QVNTS_PEDIDO.insertar_pedido( 
-        Q_PEDIDO_PEDIDO => 1,
+        Q_PEDIDO_PEDIDO => v_PEDIDO_PEDIDO;
         Q_PEDIDO_CLNT => 1,
         Q_PEDIDO_FCREA => CURRENT_timestamp
         );
+		DBMS_OUTPUT.PUT_LINE('v_PEDIDO_PEDIDO-------------------:'||v_PEDIDO_PEDIDO); 
 END;
