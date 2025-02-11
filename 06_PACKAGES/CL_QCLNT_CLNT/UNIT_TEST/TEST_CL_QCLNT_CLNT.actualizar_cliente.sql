11dc11c66ef2215667b26be0453d788006a7c214
DanielRomj <daniel.romero@ownk.co>
2025-02-11 11:35:39 -0500

    Correccion Test, Actualizar cliente

--- 06_PACKAGES/CL_QCLNT_CLNT/UNIT_TEST/TEST_CL_QCLNT_CLNT.actualizar_cliente.sql
+++ 06_PACKAGES/CL_QCLNT_CLNT/UNIT_TEST/TEST_CL_QCLNT_CLNT.actualizar_cliente.sql
@@ -1,23 +1,11 @@
-DECLARE
-    
-    v_clnt_clnt CL_TCLNT_CLNT.CLNT_CLNT%TYPE;
-    
-BEGIN
-    
-    
-    v_clnt_clnt := 1:
-    
+
+BEGIN  
     -- Insertar un nuevo cliente
     CL_QCLNT_CLNT.actualizar_cliente( 
-        P_CLNT_CLNT => v_clnt_clnt,              --OUT      CL_TCLNT_CLNT.CLNT_CLNT%TYPE,
-        P_CLNT_NOMB => 'Katy Perry',             --IN       CL_TCLNT_CLNT.CLNT_NOMB%TYPE,
+        P_CLNT_CLNT => 3,                        --IN      CL_TCLNT_CLNT.CLNT_CLNT%TYPE,
+        P_CLNT_NOMB => 'Katy Houston',           --IN       CL_TCLNT_CLNT.CLNT_NOMB%TYPE,
         P_CLNT_TPID => 'CC',                     --IN       CL_TCLNT_CLNT.CLNT_TPID%TYPE,
-        P_CLNT_NIT => 123455432,                 --IN       CL_TCLNT_CLNT.CLNT_NIT%TYPE,
-        P_CLNT_DIRE => 'Direccion prueba 1'      --IN       CL_TCLNT_CLNT.CLNT_DIRE%TYPE
-    );
-    
-    DBMS_OUTPUT.PUT_LINE('v_clnt_clnt-------------------:'||v_clnt_clnt);     
-               
-    
-    
+        P_CLNT_NIT => 201455432,                 --IN       CL_TCLNT_CLNT.CLNT_NIT%TYPE,
+        P_CLNT_DIRE => 'Direccion '              --IN       CL_TCLNT_CLNT.CLNT_DIRE%TYPE
+    );                
 END;
